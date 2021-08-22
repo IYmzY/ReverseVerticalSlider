c0d50aed3e4f8723868f5df2113b46cd611ae4be
YmzY <redanabil2199@gmail.com>
2021-08-23 00:52:44 +0200

    rightSlide on

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,19 @@ const slidesLength = rightSlide.querySelectorAll('div').length
 
 let activeSlideIndex = 0
 
-leftSlide.getElementsByClassName.top = `-${(slidesLength - 1)}`
+leftSlide.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    }
+
+    rightSlide.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+}
